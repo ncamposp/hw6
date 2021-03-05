2c600b1243da66cf2884ad444c3f02aa1b054ca8
ncamposp <>
2021-03-04 23:41:32 -0800

    Third Test, test both 3 and 5

--- test_FizzBuzz.py
+++ test_FizzBuzz.py
@@ -11,6 +11,11 @@ class TestFizzBuzz (unittest.TestCase):
     self.assertEqual(FizzBuzz.FizzBuzz(3), "Fizz")
     self.assertEqual(FizzBuzz.FizzBuzz(99), "Fizz")
     self.assertEqual(FizzBuzz.FizzBuzz(66), "Fizz")
+
+    self.assertEqual(FizzBuzz.FizzBuzz(60), "FizzBuzz")
+    self.assertEqual(FizzBuzz.FizzBuzz(90), "FizzBuzz")
+    self.assertEqual(FizzBuzz.FizzBuzz(90), "FizzBuzz")
+
     
 if __name__ == '__main__':
   unittest.main()
