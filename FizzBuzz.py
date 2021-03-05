8d572b55553121f82a63baadfce12fb24a60316c
ncamposp <>
2021-03-04 23:42:31 -0800

    Implemented FizzBuzz 3 and 5 Method; return 'FizzBuzz' if input is multiple of 5 and 3

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,6 +1,7 @@
 def FizzBuzz(x):
-    if x % 5 == 0:
-        return "Buzz"
+    if x % 5 == 0 and x % 3 == 0:
+        return "FizzBuzz"
     elif x % 3 == 0:
         return "Fizz"
-  
+    elif x % 5 == 0:
+        return "Buzz"
